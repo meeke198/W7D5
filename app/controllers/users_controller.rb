5d9e790d47cc0d684cc81388f8474a02f422f6a8
hien nguyen <thuhiennguyen198@gmail.com>
2021-09-24 12:07:31 -0400

    create routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,3 @@
+class Userscontroller < ApplicationController
+    
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,28 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  username        :string           not null
+#  password_digest :integer          not null
+#  session_token   :integer          not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 class User < ApplicationRecord
+    validates :username, :session_token, presence: true, uniqueness: true
+    validates :password_digest, presence: true
+    validates :password, length: {minimum: 6, allow_nil: true}
+
+    has_many :posts,
+    primary_key: :id,
+    foreign_key: :moderator_id,
+    class_name: :Post 
+
+    has_many :subs,
+    primary_key: :id,
+    foreign_key: :author_id,
+    class_name: :Sub 
+
+
 end
